02434a961f29189c00fb9f0cf98ef7f2cc0c891c
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-31 15:01:22 +0530

    Added: Create a queue and do enqueue operation

---
+++ src/com/bridgelabz/datastructures/QueueMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.datastructures;
+
+public class QueueMain {
+    public static void main(String[] args) {
+    Queue<Integer> integerQueue = new Queue<>();
+
+        integerQueue.enqueue(78);
+        integerQueue.enqueue(56);
+        integerQueue.enqueue(30);
+        integerQueue.enqueue(70);
+
+    }
+}
